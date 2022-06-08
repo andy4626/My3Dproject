c2f3e9be1c5099cb594a92022181f4a476b192b5
Andrew Kim <andrew.kdy@gmail.com>
2022-06-07 21:47:56 -0400

    adding new line of code

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -9,6 +9,7 @@ public class HelloWorld : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World");
+        Debug.Log("Making change for develop branch");
     }
 
     // Update is called once per frame
